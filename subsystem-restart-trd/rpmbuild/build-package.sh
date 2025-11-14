88aa648335ac3f2e83397ba1d24a46be684d99e3
Kush Jain <kush.jain@amd.com>
2025-11-14 00:27:04 -0800

    vek385: updated copyright banners
    - added latest '2025' copy-right banners across entire vek385 TRD
      artifacts
    
    Signed-off-by: Kush Jain <kush.jain@amd.com>

--- subsystem-restart-trd/rpmbuild/build-package.sh
+++ subsystem-restart-trd/rpmbuild/build-package.sh
@@ -1,5 +1,28 @@
 #!/bin/bash
 
+###############################################################################
+#
+# Copyright (C) 2025 Advanced Micro Devices, Inc. All Rights Reserved.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+###############################################################################
+
 GREEN='\033[0;32m'
 RED='\033[0;31m'
 YELLOW='\033[0;33m'
@@ -16,7 +39,6 @@ PLATFORM="versal_2ve_2vm"
 PACKAGE_NAME="subsys-restart-app"
 PACKAGE_VERSION="2025.2"
 PACKAGE_RELEASE="1"
-# SOURCE_DIR="BUILD/${PACKAGE_NAME}-${PACKAGE_VERSION}"
 SOURCE_DIR="BUILD/"
 TARBALL="SOURCES/${PACKAGE_NAME}-${PACKAGE_VERSION}.tar.gz"
 SPEC_FILE="SPECS/${PACKAGE_NAME}.spec"
